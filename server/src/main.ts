e77a3ea418a2ae3747830246080fef39df9bd468
zhirschtirtt <zhirschtritt@gmail.com>
2018-12-23 09:48:59 -0500

    fix: swagger docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  const options = new DocumentBuilder()
+    .setTitle('CheckIn')
+    .setDescription('Attendance and Location Tracking')
+    .setVersion('1.0')
+    .setBasePath('api')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('/swagger', app, document);
   await app.listen(3000);
 }
+
 bootstrap();
