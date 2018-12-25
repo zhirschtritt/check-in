001fd3c6b0ebdd58db0d87332930c2a85aec7c72
zhirschtirtt <zhirschtritt@gmail.com>
2018-12-25 15:19:32 -0500

    fix: use typeorm repo

--- server/src/kids/commands/handlers/check-in.handler.ts
+++ server/src/kids/commands/handlers/check-in.handler.ts
@@ -1,22 +1,21 @@
 import * as clc from 'cli-color';
-import { EventPublisher, ICommandHandler, CommandHandler } from '@nestjs/cqrs';
-import { KidRepository } from '../../repository/kid.repository';
-import { CheckInCommand } from '../impl/check-in.command';
+import {EventPublisher, ICommandHandler, CommandHandler} from '@nestjs/cqrs';
+import {CheckInCommand} from '../impl/check-in.command';
+import {KidsService} from '../../kids.service';
+import {Kid} from '../../models/kid.model';
 
 @CommandHandler(CheckInCommand)
 export class CheckInHandler implements ICommandHandler<CheckInCommand> {
   constructor(
-    private readonly repository: KidRepository,
+    private readonly kidService: KidsService,
     private readonly publisher: EventPublisher,
   ) {}
 
   async execute(command: CheckInCommand, resolve: (value?) => void) {
     console.log(clc.greenBright('CheckInCommand handled...')); // tslint:disable-line
+    const {kidId, locationId} = command;
+    const kid = this.publisher.mergeObjectContext(new Kid(kidId));
 
-    const { kidId, locationId } = command;
-    const kid = this.publisher.mergeObjectContext(
-      await this.repository.findOneById(+kidId),
-    );
     kid.checkIn(locationId);
     kid.commit();
     resolve();
--- server/src/kids/repository/fixtures/kids.ts
+++
@@ -1,3 +0,0 @@
-import { Kid } from '../../models/kid.model';
-
-export const testKid = new Kid('1234');
