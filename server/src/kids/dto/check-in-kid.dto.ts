a26075f62247f6470cef2e21164880c87dd25723
zhirschtirtt <zhirschtritt@gmail.com>
2018-12-25 15:18:53 -0500

    fix: swagger setup

--- server/src/kids/dto/check-in-kid.dto.ts
+++ server/src/kids/dto/check-in-kid.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty } from 'class-validator';
+import {IsNotEmpty} from 'class-validator';
+import {ApiModelProperty} from '@nestjs/swagger';
 
 export class CheckInKidDto {
-
+  @ApiModelProperty({required: true})
   @IsNotEmpty()
   readonly locationId: string;
 }
--- server/src/kids/dto/create-kid.dto.ts
+++ server/src/kids/dto/create-kid.dto.ts
@@ -1,13 +1,16 @@
-import { IsNotEmpty } from 'class-validator';
+import {IsNotEmpty} from 'class-validator';
+import {ApiModelProperty} from '@nestjs/swagger';
 
 export class CreateKidDto {
-
+  @ApiModelProperty({required: true})
   @IsNotEmpty()
   readonly firstName: string;
 
+  @ApiModelProperty({required: true})
   @IsNotEmpty()
   readonly lastName: string;
 
+  @ApiModelProperty({required: true, type: 'string'})
   @IsNotEmpty()
   readonly dob: Date;
 }
