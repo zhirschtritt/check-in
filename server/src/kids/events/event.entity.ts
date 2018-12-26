8b60db1fb9add2b0af3b63f5a4ca71e3ee0d4ba3
zhirschtirtt <zhirschtritt@gmail.com>
2018-12-26 09:41:44 -0500

    fix: type event name

--- server/src/kids/events/event.entity.ts
+++ server/src/kids/events/event.entity.ts
@@ -5,13 +5,17 @@ import {
   CreateDateColumn,
 } from 'typeorm';
 
+export enum EventType {
+  checkIn = 'CHECK-IN',
+}
+
 @Entity()
 export class EventEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
-  name: string;
+  @Column('enum')
+  event_type: EventType;
 
   @Column('simple-json')
   data: object;
--- server/src/kids/events/handlers/kid-checked-in.handler.ts
+++ server/src/kids/events/handlers/kid-checked-in.handler.ts
@@ -2,7 +2,7 @@ import * as clc from 'cli-color';
 import {IEventHandler, EventsHandler} from '@nestjs/cqrs';
 import {KidCheckedInEvent} from '../impl/kid-checked-in.event';
 import {Repository} from 'typeorm';
-import {EventEntity} from '../event.entity';
+import {EventEntity, EventType} from '../event.entity';
 import {validate} from 'class-validator';
 import {InjectRepository} from '@nestjs/typeorm';
 
@@ -13,18 +13,18 @@ export class KidCheckedInHandler implements IEventHandler<KidCheckedInEvent> {
     private readonly eventRepository: Repository<EventEntity>,
   ) {}
   async handle(event: KidCheckedInEvent) {
-    const newCheckInEvent = new EventEntity();
+    const newCheckIn = new EventEntity();
 
-    newCheckInEvent.name = 'check-in';
-    newCheckInEvent.data = event;
+    newCheckIn.event_type = EventType.checkIn;
+    newCheckIn.data = event;
 
-    const errors = await validate(newCheckInEvent);
+    const errors = await validate(newCheckIn);
 
     if (errors.length > 0) {
       const _errors = 'Event is not valid.';
       throw new Error(_errors);
     } else {
-      this.eventRepository.save(newCheckInEvent);
+      this.eventRepository.save(newCheckIn);
       console.log(clc.greenBright('KidCheckedInEvent handled...')); // tslint:disable-line
     }
   }
